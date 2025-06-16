0d46a5fe5d5a10846244070cbb92585d70cc967c
Lorenzo Sacco <lollipopsacco@gmail.com>
2025-06-16 13:15:18 +0200

    Autenticazione, da rifinire

---
+++ src/main/java/it/uniroma3/siw/service/UserService.java
@@ -0,0 +1,35 @@
+package it.uniroma3.siw.service;
+
+import it.uniroma3.siw.model.User;
+import it.uniroma3.siw.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    public User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && !(authentication instanceof AnonymousAuthenticationToken)) {
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof UserDetails) {
+                String username = ((UserDetails) principal).getUsername();
+                Optional<User> userOpt = userRepository.findByUsername(username);
+                return userOpt.orElse(null);
+            }
+        }
+
+        return null;
+    }
+}
